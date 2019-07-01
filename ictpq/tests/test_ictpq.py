d2953bfb058c3fd93d59c0d2f0409b58dcf139b1
Nathan Shammah <nathan.shammah@gmail.com>
2019-07-01 22:36:35 +0200

    fix indentation

--- ictpq/tests/test_ictpq.py
+++ ictpq/tests/test_ictpq.py
@@ -5,9 +5,9 @@ from numpy.testing import assert_, assert_equal, assert_almost_equal
 from ictpq import purity
 
 def test_purity():
-"""
-Thest the `purity` function for a normalized quantum state. 
-"""
+	"""
+	Test the `purity` function for a normalized quantum state. 
+	"""
 	psi = qt.basis(3,1)
 	rho = qt.ket2dm(psi)
 	assert_equal(purity(rho),1.)
