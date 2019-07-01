007cd9b620c9a182a2836fbfc23673898488bf13
Nathan Shammah <nathan.shammah@gmail.com>
2019-07-01 22:33:00 +0200

    add test

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+"""
+Setup file created
+"""
+from setuptools import setup
+
+setup(name='ictp-quantum',
+      version='0.1',
+      description='Open-Source Quantum Physics at the ICTP',
+      url='https://github.com/nathanshammah/ictp-quantum',
+      author='Nathan Shammah',
+      license='BSD 3 New',
+      packages = ['ictp-quantum']
+     )
