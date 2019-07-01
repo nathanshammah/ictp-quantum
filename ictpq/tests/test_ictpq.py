143d34e20aa7807aa318056b0fa32300ca0fc7c3
Nathan Shammah <nathan.shammah@gmail.com>
2019-07-01 22:35:04 +0200

    update name library

---
+++ ictpq/tests/test_ictpq.py
@@ -0,0 +1,13 @@
+import numpy as np
+import scipy as sc
+import qutip as qt
+from numpy.testing import assert_, assert_equal, assert_almost_equal
+from ictpq import purity
+
+def test_purity():
+"""
+Thest the `purity` function for a normalized quantum state. 
+"""
+	psi = qt.basis(3,1)
+	rho = qt.ket2dm(psi)
+	assert_equal(purity(rho),1.)
