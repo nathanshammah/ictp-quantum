c74900bee8d326cfbeb034533343c97fb762613d
Nathan Shammah <nathan.shammah@gmail.com>
2019-07-01 22:23:23 +0200

    add function

--- ictp-quantum/ictp-quantum.py
+++ ictp-quantum/ictp-quantum.py
@@ -2,3 +2,21 @@ import numpy as np
 import scipy as sc
 import qutip as qt
 import matplotlib.pyplot as plt
+
+
+def purity(rho):
+"""
+Calculate the purity of a quantum state (normalized in the process). 
+
+Parameters
+----------
+rho : :class:`qutip.Qobj`
+	Density matrix of a quantum system.
+
+Returns
+----------
+purity_rho : float
+	Purity of the quantum system, p (p=1 if pure, 1/N<=p<1 if mixed).
+"""
+	purity_rho = ((rho*rho).unity()).tr()
+	return purity_rho
